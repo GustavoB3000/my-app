959984eb61120a39d415bf8ba6afe91db5151854
Gustavo Bearzi <gustavobearzi2012@gmail.com>
2023-02-20 01:17:02 -0300

    Nombre del proyecto: my-app. Descripcion: version final del proyecto my-app

---
+++ src/main/java/com/gustavo/myapp/entity/TestEntity.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.gustavo.myapp.entity;
+
+/**
+ * @author Calistos
+ *
+ */
+public class TestEntity {
+	/**
+	 * Propiedad de prueba para la muestra de compilacion del archivo testentity.
+	 */
+	private String name;
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
